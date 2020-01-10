8e80dd96c7c37c32413fd618ce6f6f235d8bff91
Timur <t.fralik@aimprosoft.com>
2020-01-10 19:30:39 +0200

    Add first test try

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker,
+        RefreshDatabase;
+
+    public function testA_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+        ];
+
+        $this->post('/project', $attributes);
+        $this->assertDatabaseHas('project', $attributes);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        //$response = $this->get('/');
+
+        //$response->assertStatus(200);
+    }
+}
