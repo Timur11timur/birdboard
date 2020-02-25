4b214df464dc54fe921644f8207701b679efa609
Timur <t.fralik@aimprosoft.com>
2020-02-25 11:24:16 +0200

    Add $loop

--- resources/views/projects/layouts/activity.blade.php
+++ resources/views/projects/layouts/activity.blade.php
@@ -1,5 +1,7 @@
 <div class="card border-0 shadow p-4 d-flex align-items-stretch rounded-lg col mt-3">
     @foreach($project->activity as $activity)
-        <p>@include("projects.activity.{$activity->description}") <span class="text-secondary">{{ $activity->created_at->diffForHumans(null, true) }}</span></p>
+        <p class="{{ $loop->last ? 'mb-0' : 'mb-2' }}">
+            @include("projects.activity.{$activity->description}") <span class="text-secondary">{{ $activity->created_at->diffForHumans(null, true) }}</span>
+        </p>
     @endforeach
 </div>
