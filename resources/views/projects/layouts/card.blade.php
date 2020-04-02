f2745deabddff4a22d612606fa2e5663f7e81d5c
Timur <timur@fralik.ru>
2020-04-03 00:23:31 +0300

    After 35 video

--- resources/views/projects/layouts/card.blade.php
+++ resources/views/projects/layouts/card.blade.php
@@ -3,11 +3,13 @@
         <h3 class="card-title ml-n4 pl-3 border-info border-left border-1 py-2" style="font-size: 1.5rem;">{{ $project->title }}</h3>
         <div class="card-text text-secondary mb-3">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
     </a>
-    <footer class="align-self-end">
-        <form method="post" action="{{ $project->path() }}" class="d-flex justify-content-end">
-            @method('DELETE')
-            @csrf
-            <button type="submit" class="btn btn-link p-0">Delete</button>
-        </form>
-    </footer>
+    @can('manage', $project)
+        <footer class="align-self-end">
+            <form method="post" action="{{ $project->path() }}" class="d-flex justify-content-end">
+                @method('DELETE')
+                @csrf
+                <button type="submit" class="btn btn-link p-0">Delete</button>
+            </form>
+        </footer>
+    @endcan
 </div>
